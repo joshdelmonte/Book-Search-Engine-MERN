162c6b0431bf86caf16a010e123fdac69f6a9d99
joshdelmonte <joshdelmonte@gmail.com>
2023-01-25 00:34:51 -0700

    Fixing App.js content

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,55 @@
+// * `App.js`: Create an Apollo Provider to make every request work with the Apollo Server.
+//
+// * `App.js`: Wrap the entire app in the Apollo `<Provider>` component so that requests can communicate with the Apollo Server.
+//
+// * `App.js`: Wrap the `<Routes>` component with the `<ApolloProvider>` component and pass in the client as a prop.
+//
+// * `App.js`: Import the `ApolloProvider` component from `@apollo/client`.
+//
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              element={<SearchBooks />}
+            />
+            <Route
+              path='/saved'
+              element={<SavedBooks />}
+            />
+            <Route
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
