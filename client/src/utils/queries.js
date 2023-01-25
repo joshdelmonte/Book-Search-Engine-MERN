6ae47dcfb69daf4c2f8fe9a59fb8f1cc7851d3c6
joshdelmonte <joshdelmonte@gmail.com>
2023-01-24 23:39:30 -0700

    Created queries and mutations in client side utils with appropriate exports

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import {gql} from '@apollo/client';
+ //* `queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
