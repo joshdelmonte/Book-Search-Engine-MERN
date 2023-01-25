510b48bdf1c21a73517d7feb8a6dc12adbc894c2
joshdelmonte <joshdelmonte@gmail.com>
2023-01-25 12:33:55 -0700

    Trying to resolve errors in index.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,95 +1,50 @@
-// TODO:
-	// * Use the Apollo `useMutation()` Hook to execute the `SAVE_BOOK` mutation in the `handleSaveBook()` function instead of the `saveBook()` function imported from the `API` file.
+import React from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
 
-	// * Make sure you keep the logic for saving the book's ID to state in the `try...catch` block! 
+import { useQuery, useMutation } from "@apollo/client";
+// Import GET_ME query from the queries.js file
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import { removeBookId } from "../utils/localStorage";
 
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import Auth from "../utils/auth";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // useQuery HOOK to execute the GET_ME query on load
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK, {
-    update(cache, { data: { removeBook } }) {
-      try {
-        // could potentially not exist yet, so wrap in a try...catch
-        const { me } = cache.readQuery({ query: GET_ME });
-        cache.writeQuery({
-          query: GET_ME,
-          data: { me: { ...me, savedBooks: [...me.savedBooks, removeBook] } },
-        });
-      } catch (e) {
-        console.error(e);
-      }
+  // useMutation HOOK (SHOULD ERROR BE REMOVED FROM THIS?)
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  
+  const userData = data?.me || {};
 
-      // update me object's cache, appending new book to the end of the array
-      const data = { ...userData };
-      data.savedBooks = data.savedBooks.filter((book) => book.bookId !== bookId);
-      setUserData(data);
-    },
-  });
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
+    
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
+    // if token exists, execute the removeBook mutation and pass in the bookId and token
     if (!token) {
       return false;
     }
-
+    // use try/catch to handle errors from the removeBook mutation
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
+    
+      console.log(data);
+      // if book successfully removes from user's account, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+  
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
